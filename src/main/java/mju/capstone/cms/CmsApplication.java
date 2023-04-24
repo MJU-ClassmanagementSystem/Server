2919831c9636e9af7b873bd8254343b53c4d52b2
bluesun147 <bluesun147@naver.com>
2023-04-24 21:09:30 +0900

    Initial Commit

---
+++ src/main/java/mju/capstone/cms/CmsApplication.java
@@ -0,0 +1,14 @@
+package mju.capstone.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class CmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mju/capstone/cms/CmsApplicationTests.java
@@ -0,0 +1,13 @@
+package mju.capstone.cms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
